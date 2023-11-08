d282ff58d480ce53ac295b6fc7ac15ee606e0835
TTruble <blazingaustin@gmail.com>
2023-11-08 09:17:01 +0200

    i dont even know brutttha

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import BaseInfo from './components/BaseInfo'
+import izmantosana from './components/Izmantosana.jsx'
 
 
 
---
+++ src/components/Izmantosana.jsx
@@ -0,0 +1,12 @@
+import styles from './Izmantosana.module.css';
+function Izmantosana() {
+  return (
+<article>
+<h1>
+    Big chungus
+</h1>
+</article>
+  )
+}
+
+export default Izmantosana
