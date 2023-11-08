6431bbe999434346e68f42f3d09bfa237a973731
TTruble <blazingaustin@gmail.com>
2023-11-08 09:42:18 +0200

    fortnite
    fixed :3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import BaseInfo from './components/BaseInfo'
-import izmantosana from './components/Izmantosana'
+import Izmantosana from './components/Izmantosana'
 import UCDBlog from './components/UCDBlog'
 
 
